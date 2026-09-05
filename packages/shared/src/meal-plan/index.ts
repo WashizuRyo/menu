7b3dd1c188f70841ee673a6cc4af01bd007acd4a
Ryo Washizu <wasizuryo@gmail.com>
2026-09-05 17:47:45 +0900

    feat: add meal plan validation schemas

---
+++ packages/shared/src/meal-plan/index.ts
@@ -0,0 +1,13 @@
+export { DEFAULT_MEAL_TYPE, MEAL_TYPES } from './constants.js'
+export {
+  createMealPlanInputSchema,
+  mealPlanDateSchema,
+  mealPlanSchema,
+  mealTypeSchema,
+} from './schema.js'
+export type {
+  CreateMealPlanInput,
+  MealPlan,
+  MealPlanDate,
+  MealType,
+} from './type.js'
